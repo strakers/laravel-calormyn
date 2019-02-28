4c987efd2e70373fdfed2b5607888434a0976b4f
Steven Straker <contact@atrakez.com>
2019-02-27 20:03:29 -0500

    Extend Blueprint class to include default timestamps

---
+++ src/Calormyn/Database/Schema/CalorBlueprint.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steven
+ * Date: 2019-02-27
+ * Time: 7:53 PM
+ */
+
+namespace Calormyn\Database\Schema;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+class CalorBlueprint extends Blueprint
+{
+
+    /**
+     * Add nullable creation and update timestamps to the table.
+     *
+     * @param  int  $precision
+     * @return void
+     */
+    public function timestamps($precision = 0)
+    {
+        $this->timestamp('created_at', $precision)->useCurrent();
+
+        $this->timestamp('updated_at', $precision)->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+    }
+}
