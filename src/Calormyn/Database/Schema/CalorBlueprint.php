f5dbcde682dcddaa1a4882b87ed68226580c3316
Steve Straker <contact@strakez.com>
2020-07-10 00:41:33 -0400

    Add static method to declare new schema builder

--- src/Calormyn/Database/Schema/CalorBlueprint.php
+++ src/Calormyn/Database/Schema/CalorBlueprint.php
@@ -10,6 +10,7 @@ namespace Calormyn\Database\Schema;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 class CalorBlueprint extends Blueprint
 {
 
@@ -26,8 +27,28 @@ class CalorBlueprint extends Blueprint
         $this->timestamp('updated_at', $precision)->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
     }
 
+    /**
+     * Add max-length utf8 encoded string field to table
+     *
+     * @param $column
+     * @return mixed
+     */
     public function fullString( $column )
     {
         return $this->string( $column, 255 )->charset('utf8');
+    }
+
+    /**
+     * Return new schema for creating table
+     *
+     * @return Schema
+     */
+    static public function getSchema()
+    {
+        $schema = DB::connection()->getSchemaBuilder();
+        $schema->blueprintResolver(function($table,$callback){
+            return new self($table,$callback);
+        });
+        return $schema;
     }
 }
